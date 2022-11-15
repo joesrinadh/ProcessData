dcb3c524f1ce15a7ade41e7c754d0022c36e86b9
Srinadh Joe <v-srjoe@microsoft.com>
2022-11-15 13:34:15 +0530

    Process data

---
+++ ReadData/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReadData
+{
+	class Program
+	{
+		/// <summary>
+		/// Maximum length of substring
+		/// </summary>
+		private static readonly int SubStrLength = 4;
+
+		/// <summary>
+		/// Relative path to input and output files
+		/// </summary>
+		private static readonly string RelativePath = "../../../";
+
+		/// <summary>
+		/// Process the data from wordstest.txt and generates uniques.txt and fullwords.txt
+		/// </summary>
+		public static void ProcessData()
+		{
+			try
+			{
+				// Start timer
+				Stopwatch stopWatch = Stopwatch.StartNew();
+
+				// Dictionary to capture unique word, fullword and count of occurances
+				Dictionary<string, KeyPair> uniqueWords = new Dictionary<string, KeyPair>();
+
+				// Read all lines from input file
+				foreach (string word in File.ReadAllLines($"{RelativePath}InputData/words.txt"))
+				{
+					int strLength = word.Length;
+
+					if (strLength >= SubStrLength)
+					{
+						if (strLength == SubStrLength && !uniqueWords.ContainsKey(word))
+						{
+							uniqueWords.Add(word, new KeyPair(word, 1));
+						}
+						else
+						{
+							// Extract possible substrings for each word and capture the occurances
+							foreach (string subStr in GetSubstrings(word, SubStrLength))
+							{
+								if (!uniqueWords.ContainsKey(subStr))
+								{
+									uniqueWords.Add(subStr, new KeyPair(word, 1));
+								}
+								else
+								{
+									uniqueWords[subStr].Count += 1;
+								}
+							}
+						}
+					}
+				}
+
+				// Filter unique substrings that occured only once and sort them
+				var result = uniqueWords.Where(v => v.Value.Count == 1).OrderBy(k => k.Key).ToDictionary(d => d.Key, d => d.Value.Word);
+
+				// Export unique and fullwords to .txt files
+				File.WriteAllLines($"{RelativePath}OutputData/uniques.txt", result.Keys);
+				File.WriteAllLines($"{RelativePath}OutputData/fullwords.txt", result.Values);
+
+				// Stop timer
+				stopWatch.Stop();
+
+				Console.WriteLine($"Processed data in: {stopWatch.ElapsedMilliseconds} ms");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Failed to process because of : " + ex.Message);
+			}
+		}
+
+		/// <summary>
+		/// Get substrings for the given string
+		/// </summary>
+		/// <param name="input">Input string</param>
+		/// <param name="subStrLength">Length of substring</param>
+		/// <returns>Returns substrings</returns>
+		public static IEnumerable<string> GetSubstrings(string input, int subStrLength)
+		{
+			for (int i = 0; i <= input.Length - subStrLength; i++)
+			{
+				yield return (input.Substring(i, subStrLength));
+			}
+		}
+
+		static void Main(string[] args)
+        {
+            // Method to process the data from words.txt
+            ProcessData();
+
+            Console.ReadLine();
+        }
+    }
+
+	/// <summary>
+	/// Class to maintain fullword and count of occurances
+	/// </summary>
+	public class KeyPair
+	{
+		public string Word { get; }
+		public int Count { get; set; }
+
+		public KeyPair(string word, int count)
+		{
+			this.Word = word;
+			this.Count = count;
+		}
+	}
+}
